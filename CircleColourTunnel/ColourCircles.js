8eb984fba6a00070478e5795285944853f4436b4
markthorne159 <thornemark58@gmail.com>
2017-07-01 20:09:37 +0100

    Add files via upload

---
+++ CircleColourTunnel/ColourCircles.js
@@ -0,0 +1,38 @@
+function DrawSpheres (D,E,F) {
+this.x = (displayWidth/2);
+this.y = (displayHeight/2);
+this.Xscale = 0;
+this.Yscale = 0;
+
+this.Full = 0;
+this.A = D;
+this.B = E;
+this.C = F;
+
+
+
+this.display = function () {
+	var Col = [
+	color(255, 0, 8),
+	color(255, 187, 0),
+	color(250, 255, 0),
+	color(50, 255, 0),
+	color(2, 175, 255),
+	color(216, 0, 202),
+	color(255, 10, 189),
+	];
+	this.ScaleAmount = 3;
+	stroke(255);
+	fill(this.A,this.B,this.C);
+	ellipse(this.x,this.y,this.Xscale,this.Yscale);
+	
+	
+		this.Xscale += this.ScaleAmount;
+		this.Yscale += this.ScaleAmount;
+	
+	
+	// else if(this.Xscale > displayWidth && this.Yscale > displayHeight){
+		// this.Full = 1;
+	// }
+}
+}
---
+++ CircleColourTunnel/sketch.js
@@ -0,0 +1,41 @@
+var Size = 20;
+var Amount = 1;
+var Done = 1;
+
+var Circles = [];
+var TotalCircles = 10;
+
+function setup() {
+myFunction();
+createCanvas(displayWidth,displayHeight);
+background(51);
+}
+
+function draw() {
+	background(51);
+	
+	if(Circles.length > 10){
+	Circles.splice(0,1);
+}
+	
+	for(var i = 0; i < Circles.length; i++){
+		Circles[i].display();
+	}
+	
+	for(var i = 0; i < Circles.length; i++){
+		if(Circles[i].Full == 1){
+		Circles.splice(i,1);
+		Amount = 1;
+		}
+	}
+	console.log("Done!");
+ }
+
+function CirclePush () {
+var A = random(0,255);
+	var B = random(0,255);
+	var C = random(0,255);	
+		Circles.push(new DrawSpheres(A,B,C));
+}
+ 
+ 
